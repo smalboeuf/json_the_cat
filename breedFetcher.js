814e8480d572bdef7b91f034d117a1747403a879
Sheldon Malboeuf <smalboeuf01@gmail.com>
2020-02-13 17:15:39 +0000

    Returns description of inputted breed

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,30 +2,33 @@
 //documentation: https://docs.thecatapi.com/api-reference/breeds/breeds-search
 
 const request = require('request');
-const fs = require('fs');
 const args = process.argv;
 
 args.splice(0, 2);
 
-const breedFetcher = function (args) {
+const breedFetcher = function(args) {
 
-  const URL = 'https://api.thecatapi.com/v1/breeds/search?q='
+
+  const URL = 'https://api.thecatapi.com/v1/breeds/search?q=';
   const BREED = args[0];
   
+  console.log(URL + BREED);
+  
+  request(URL + BREED, (error, response, body) => {
 
-  request(URL+BREED, (error, response, body) => {
-    if (error !== undefined) {
-      console.log(error);
-    } else {
-
+    try {
       let data = JSON.parse(body);
       data = data[0];
-      console.log(data["weight"]);
+      console.log(data.description);
+    } catch (e) {
+      console.log(e.name);
     }
   
-});
+  });
 
-}
+
+  return "searching...";
+};
 
 
 
